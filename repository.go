b13f97fff9fafafde8c3126e23bb8cc638674a57
bunyaminkorkut <krktbunyamin@gmail.com>
2025-07-20 00:19:43 +0300

    fix

--- repository.go
+++ repository.go
@@ -11,6 +11,6 @@ type UserRepository struct {
 
 func (repo *UserRepository) CreateUser(id, username, email, hashedPassword string, iban string) error {
 	log.Println("Kullanıcı oluşturuluyor:", id, username, email, hashedPassword, iban)
-	_, err := repo.DB.Exec("INSERT INTO users (id, fullname, email, password_hash, iban) VALUES (?, ?, ?, ?, ?)", id, username, email, hashedPassword)
+	_, err := repo.DB.Exec("INSERT INTO users (id, fullname, email, password_hash, iban) VALUES (?, ?, ?, ?, ?)", id, username, email, hashedPassword, iban)
 	return err
 }
